ab3d604bcf86e2ff82721a48a4c3e9c7ba8d90fb
pyyding <kaspar.pyyding@gmail.com>
2018-08-09 19:26:27 +0300

    update answers, questions, when user changes something

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # producer-chat-slackapp
 Google Functions for Producer Chat
+
+
+## Configuring app
+
+#### init config
+firebase functions:config:set slack.key="a9IboJLx1WcMIDiazPcpTizn" slack.id="397383219366.395984546068" slack.access_token="xoxp-397383219366-396043467059-395985380612-ade3572d730795df75cd83e04b17406f" slack.bot_access_token="xoxb-397383219366-395985384388-5n3T12ArBrwtgwyR06zCzLHB"
+
+#### run app locally
+
+1. get config: `firebase functions:config:get > .runtimeconfig.json` (run in functions directory)
+2. npm run lint
+3. npm run build
+4. firebase serve —only functions
+5. ngrok http 5000
+6. set request urls to ngrok urls : https://d21e59af.ngrok.io/producer-chat/us-central1/message_action
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,9 +6,9 @@ admin.initializeApp(functions.config().firebase);
 const { WebClient } = require('@slack/client');
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
-const token = functions.config().slack.key;
+const key = functions.config().slack.key;
 
-const slack = new WebClient(token);
+const slack = new WebClient(key);
 
 
 const db = admin.firestore();
@@ -20,10 +20,22 @@ export const message_action = functions.https.onRequest(async (request, response
       return response.status(200).send(request.body.challenge)
     }
     case 'event_callback': {
-      const { event } = request.body; // @TODO: verify with token
-      console.log(event);
-      switch (event.type) {
+        if (request.method !== "POST") {
+            console.error(`Got unsupported ${request.method} request. Expected POST.`);
+            return response.status(405).send("Only POST requests are accepted");
+        }
+
+        if (request.body.token !== functions.config().slack.key) {
+            console.error(`Invalid request token ${request.body.token} from ${request.body.event.user}`);
+            return response.status(401).send("Invalid request token!");
+        }
+
+    console.log('action event type: ' + request.body.event.type);
+
+      switch (request.body.event.type) {
         case 'message': {
+        const action = request.body as SlackMessageAction;
+
           const welcomeTextWeb = "1. Upload your avatar image to Slack.\n" +
             "2. Log in to the web app here  👉 httos://producer.chat/login\n" +
             "3. Ask the community from here 👉 https://producer.chat/qa";
@@ -31,7 +43,7 @@ export const message_action = functions.https.onRequest(async (request, response
           const welcomeTextSlack = "- Post your introduction to #general channel\n" +
             "- Join any channel you want from the little '+' button on the left sidebar of Slack\n";
           const message = {
-            token: 'xoxb-397383219366-395985384388-5n3T12ArBrwtgwyR06zCzLHB',
+            token: functions.config().slack.bot_access_token,
             as_user: true,
             link_names: true,
             text: 'Welcome to Producer Chat! We\'re glad you\'re here.',
@@ -49,7 +61,7 @@ export const message_action = functions.https.onRequest(async (request, response
               channel: ''
           };
 
-          message.channel = event.user;
+          message.channel = action.event.user;
 
           const params = qs.stringify(message);
           console.info(params);
@@ -57,8 +69,51 @@ export const message_action = functions.https.onRequest(async (request, response
           axios.post('https://slack.com/api/chat.postMessage', params);
           return response.status(200).send()
         }
-        case 'team_join': {
-          return response.status(200).send({ text: 'cool message sent to me' })
+        case 'user_change': {
+            const action = request.body as SlackUserChangeAction;
+            console.info('triggered user change: ' + action.event.user.id  );
+            const slackResponse = await slack.users.info({ user: action.event.user.id, token: functions.config().slack.access_token });
+            if (slackResponse.ok) {
+                console.log('slackUser found, commiting changes');
+                const slackUser = slackResponse.user as SlackUser;
+                const userRef = db.collection('users').doc(action.event.user.id);
+                userRef.update({
+                    displayName: slackUser.profile.display_name,
+                    photoURL: slackUser.profile.image_72
+                });
+                const newUser = {
+                    user: {
+                        id: slackUser.id,
+                        displayName: slackUser.profile.display_name,
+                        photoURL: slackUser.profile.image_72
+                    }
+                };
+
+                const questionsRef = await db.collection('questions').where('user.id', '==', action.event.user.id).get();
+                const questionIDs = [];
+                questionsRef.forEach(question => {
+                    questionIDs.push(question.id);
+                });
+
+                const answersRef = await db.collection('answers').where('user.id', '==', action.event.user.id).get();
+                const answerIDs = [];
+                answersRef.forEach(answer => {
+                    answerIDs.push(answer.id);
+                });
+
+                const batch = db.batch();
+                questionIDs.forEach(id => {
+                   const questionRef = db.collection('questions').doc(id);
+                   batch.update(questionRef, newUser)
+                });
+                answerIDs.forEach(id => {
+                   const answerRef = db.collection('answers').doc(id);
+                   batch.update(answerRef, newUser)
+                });
+                batch.commit();
+                return response.status(200).send();
+            }
+            return response.status(404).send();
         }
       }
     }
@@ -89,7 +144,7 @@ export const command_ping = functions.https.onRequest(async (request, response) 
 
 export const trigger_create_user = functions.auth.user().onCreate((user) => {
     console.log('triggered create_user');
-    return slack.users.lookupByEmail({email: user.email, token: 'xoxp-397383219366-396043467059-395985380612-ade3572d730795df75cd83e04b17406f'})
+    return slack.users.lookupByEmail({email: user.email, token: functions.config().slack.access_token })
         .then(function (response) {
             console.log('slack response:' + response);
            if (response.ok) {
@@ -98,10 +153,10 @@ export const trigger_create_user = functions.auth.user().onCreate((user) => {
                const newUser = {
                    email: user.email,
                    displayName: slackUser.profile.display_name,
-                   photoURL72: slackUser.profile.image_72,
-                   photoURL192: slackUser.profile.image_192,
+                   photoURL: slackUser.profile.image_72,
                    isAdmin: slackUser.is_admin,
-                   isRestricted: slackUser.is_restricted
+                   isRestricted: slackUser.is_restricted,
+                   createdAt: new Date()
                };
                console.log(newUser);
                db.collection('users').doc(slackUser.id).set(newUser)
@@ -110,100 +165,6 @@ export const trigger_create_user = functions.auth.user().onCreate((user) => {
            }
         });
 });
-
-// export const command_ask = functions.https.onRequest(async (request, response) => {
-//     if (request.method !== "POST") {
-//         console.error(`Got unsupported ${request.method} request. Expected POST.`);
-//         return response.status(405).send("Only POST requests are accepted");
-//     }
-//
-//     const command = request.body as SlackSlashCommand;
-//     if (command.token !== functions.config().slack.key) {
-//         console.error(`Invalid request token ${command.token} from ${command.team_id} (${command.team_domain}.slack.com)`);
-//         return response.status(401).send("Invalid request token!");
-//     }
-//
-//     const question = {
-//         title: command.text,
-//         user: command.user_id,
-//     }
-//
-//     admin.database().ref('/questions').push
-//
-// });
-
-// export const command_get_login_link = functions.https.onRequest(async (request, response) => {
-//     if (request.method !== "POST") {
-//         console.error(`Got unsupported ${request.method} request. Expected POST.`);
-//         return response.status(405).send("Only POST requests are accepted");
-//     }
-//
-//     const command = request.body as SlackSlashCommand;
-//     if (command.token !== functions.config().slack.key) {
-//         console.error(`Invalid request token ${command.token} from ${command.team_id} (${command.team_domain}.slack.com)`);
-//         return response.status(401).send("Invalid request token!");
-//     }
-//
-//     const authToken = Math.random().toString(36).substring(2, 8) + Math.random().toString(36).substring(2, 8);
-//
-//     const newUser = {
-//         displayName: '',
-//         photoURL: '',
-//         authToken: authToken
-//     };
-//
-// const userRef = db.collection('users').doc(command.user_id);
-//     userRef.get()
-//         .then(doc => {
-//             if (doc.exists) {
-//               const existingUser = doc.data();
-//               userRef.update({ authToken: authToken });
-//               console.log('User:', existingUser);
-//             } else {
-//               console.log('creating user');
-//                 db.collection('users').doc(command.user_id).set(newUser)
-//             }
-//         })
-//         .catch(err => {
-//             console.log('Error getting document', err);
-//         });
-//
-//     return response.contentType("json").status(200).send({
-//         "response_type": "ephemeral",
-//         "text": `Click to login to the web-app: https://producer.chat/login?token=${authToken}`
-//     });
-// });
-//
-// export const command_login = functions.https.onRequest(async (request, response) => {
-//     if (request.method !== "POST") {
-//         console.error(`Got unsupported ${request.method} request. Expected POST.`);
-//         return response.status(405).send("Only POST requests are accepted");
-//     }
-//
-//     const loginModel = request.body as LoginModel;
-//
-//     try {
-//         const user = await db.collection('users').where('authToken', '==', loginModel.authToken).get()
-//     } catch (error) {
-//         console.log(error)
-//     }
-//
-//
-//
-//
-//     return response
-// });
-//
-// interface LoginModel {
-//     authToken: string
-// }
-
-interface SlackUser {
-    id: string,
-    team_id: string,
-    name: string,
-
-}
 
 interface SlackUser {
     id: string;
@@ -258,4 +219,50 @@ interface SlackSlashCommand {
   command: string,
   text: string,
   response_url: string
+}
+
+export interface SlackMessageAction {
+    token: string;
+    team_id: string;
+    api_app_id: string;
+    event: {
+        type: string,
+        user: string,
+        item: {
+            type: string,
+            channel: string,
+            ts: string
+        },
+        reaction: string,
+        item_user: string,
+        event_ts: string
+    };
+    type: string;
+    authed_users: string[];
+    event_id: string,
+    event_time: number
+}
+
+export interface SlackUserChangeAction {
+    token: string;
+    team_id: string;
+    api_app_id: string;
+    event: {
+        type: string,
+        user: {
+            id: string
+        },
+        item: {
+            type: string,
+            channel: string,
+            ts: string
+        },
+        reaction: string,
+        item_user: string,
+        event_ts: string
+    };
+    type: string;
+    authed_users: string[];
+    event_id: string,
+    event_time: number
 }
