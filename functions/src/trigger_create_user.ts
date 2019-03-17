63d618ffdcccc7298ee6b6ecbc89a35fe6ea2c11
pyyding <kaspar.pyyding@gmail.com>
2019-03-17 19:44:40 +0200

    create user with slugs

--- functions/src/trigger_create_user.ts
+++ functions/src/trigger_create_user.ts
@@ -1,30 +1,41 @@
 import * as functions from "firebase-functions";
-import {SlackUser} from "./interfaces";
+import {COLLECTIONS} from "./constants";
 import slugify from "slugify";
 
 exports.handler = async function (user, db, slack) {
-    console.info("triggered create_user");
-    console.info("user: " + user.email);
-    console.info(slack.users);
-    console.info(slack.users.lookupByEmail);
-    const slackResponse = await slack.users.lookupByEmail({email: user.email, token: functions.config().slack.access_token });
-    if (slackResponse.ok) {
+    console.info(`user: ${user.email}`);
+    try {
+        const slackResponse = await slack.users.lookupByEmail({
+            email: user.email,
+            token: functions.config().slack.access_token
+        });
+        if (!slackResponse.ok) {
+            console.error("Couldn't find slack user");
+            return false;
+        }
         console.info("found slack user");
-        const slackUser = slackResponse.user as SlackUser;
+        const { user: slackUser } = slackResponse;
+        const displayName = slackUser.profile.display_name;
+        let slug = slugify(slackUser.profile.display_name);
+        const usersWithSameSlug = await db.collection(COLLECTIONS.USERS).where("displayName", "==", displayName).get();
+        const numberOfDuplicates = usersWithSameSlug.docs.length;
+        if (numberOfDuplicates > 0) {
+            slug = `${slug}-${numberOfDuplicates}`;
+        }
+
         const newUser = {
             email: user.email,
-            displayName: slackUser.profile.display_name,
+            displayName,
             photoURL: slackUser.profile.image_72,
             isAdmin: slackUser.is_admin,
             isRestricted: slackUser.is_restricted,
-            slug: slugify(slackUser.profile.display_name),
+            slug,
             createdAt: new Date()
         };
-        console.log(newUser);
-        db.collection("users").doc(slackUser.id).set(newUser);
+        db.collection(COLLECTIONS.USERS).doc(slackUser.id).set(newUser);
         return true;
-    } else {
-        console.error("no slack user found.");
+    } catch (e) {
+        console.error(e);
         return false;
     }
 };
